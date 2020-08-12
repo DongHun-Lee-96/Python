faceb5d6307ddebf3c810d37ba9f78c4e2844c94
DongHun Lee <ldh960102@gmail.com>
2020-08-12 11:57:26 +0900

    Module

---
+++ Mod2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Module
+
+# In[1]:
+
+
+PI = 3.141592
+
+class Math: 
+    def solv(self, r): 
+        return PI * (r ** 2) 
+
+def add(a, b): 
+    return a+b 
+
